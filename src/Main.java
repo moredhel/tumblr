5e8c167bf5edc55153d4b7a3da536acd0c8d323d
moredhel <hamhut1066@gmail.com>
2014-03-16 14:40:08 +0000

    add class for all things tumblr=

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,15 @@
 import com.tumblr.jumblr.JumblrClient;
 import com.tumblr.jumblr.types.Blog;
 import com.tumblr.jumblr.types.User;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.util.List;
-import javax.swing.*;
+import forms.frmMain;
 
 /**
  * Created by moredhel on 16/03/14.
  */
 public class Main {
-    public static void createAndShowGUI(){
-        JFrame frame = new JFrame("HelloWorldSwing");
-
-        //finish initial config of swing gui -----------------------------
-        JMenuBar menubar = new JMenuBar();
-        JPanel panel = new JPanel();
-        //panel.setBounds(10,10,300,10);
-        panel.setLayout(null);
-        JMenu file = new JMenu("File");
-        file.setMnemonic(KeyEvent.VK_F);
-
-        JMenuItem eMenuItem = new JMenuItem("Exit");
-        eMenuItem.setMnemonic(KeyEvent.VK_E);
-        eMenuItem.setToolTipText("Exit application");
-        eMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.exit(0);
-            }
-        });
-        panel.add(menubar);
-        menubar.add(file);
-        file.add(eMenuItem);
-        frame.add(panel);
-        //start final config of swing gui --------------------------------
-        frame.setSize(300, 200);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    public static frmMain main;
+    public static  void createAndShowGUI(){
+        main = new frmMain();
     }
 
     public static void main(String args[]){
